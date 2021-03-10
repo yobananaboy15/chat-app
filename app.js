131bb34c7c31cd0b89365eddfd0692b3bca0baab
Axel Thiel <axth2220@gmail.com>
2021-03-10 11:12:20 +0100

    fixed bug that enables starting PM with yourself

--- app.js
+++ app.js
@@ -91,28 +91,34 @@ io.on("connect", (socket) => {
   socket.on('startPM', async (username) => {
     const userDocument = await Users.findOne({username: username})
     const userID = userDocument._id
-    const privateConvo = await Channels.findOne({users: {$all: [userID, usersData[socket.id]._id]}})
-    if(!privateConvo){
-      //Skapa ett dokument med ett nytt rum som har båda users i sin userArray.
-      const newChannel = new Channels({
-        channelname: 'test',
-        private: true,
-        messages: [],
-        users: [userID, usersData[socket.id]._id]
-      })
-      const newchan = await newChannel.save();
 
-      //Kolla om den andra användaren är online. Om hen är det, skicka den nya privata kanel
-      let userdata = Object.entries(usersData)
-      const onlineUser = userdata.find(user => user[1]._id == userID)
-      if(onlineUser){
-        io.to(onlineUser[0]).emit('channelAdded', newchan)
-      }
-      //Redirecta användaren som klickade på PM
-      io.to(socket.id).emit('redirect', `/chat/${newchan._id}`)
+    //Can't start a conversation with yourself
+    console.log(userID, usersData[socket.id]._id)
+    console.log(userID != usersData[socket.id]._id)
+    if(userID != usersData[socket.id]._id){
+      const privateConvo = await Channels.findOne({users: {$all: [userID, usersData[socket.id]._id]}})
+      if(!privateConvo){
+        //Skapa ett dokument med ett nytt rum som har båda users i sin userArray.
+        const newChannel = new Channels({
+          channelname: 'test',
+          private: true,
+          messages: [],
+          users: [userID, usersData[socket.id]._id]
+        })
+        const newchan = await newChannel.save();
+
+        //Kolla om den andra användaren är online. Om hen är det, skicka den nya privata kanel
+        let userdata = Object.entries(usersData)
+        const onlineUser = userdata.find(user => user[1]._id == userID)
+        if(onlineUser){
+          io.to(onlineUser[0]).emit('channelAdded', newchan)
+        }
+        //Redirecta användaren som klickade på PM
+        io.to(socket.id).emit('redirect', `/chat/${newchan._id}`)
     }
     io.to(socket.id).emit('redirect', `/chat/${privateConvo._id}`)
-  
+    }
+    
   })
 
   socket.on('disconnect', () =>{
