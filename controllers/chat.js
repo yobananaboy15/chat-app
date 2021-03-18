332f10fee6cbb923ba141f2319f69ad20037d510
Axel Thiel <axth2220@gmail.com>
2021-03-18 12:21:50 +0100

    fixed redirect if channel doesn't exist

--- controllers/chat.js
+++ controllers/chat.js
@@ -21,20 +21,27 @@ export const renderChat = async (req, res) => {
   const loggedInUser = req.user._id
   const channelID = req.params.id
 
-  //Find all public channels to display
+  try {
+    const currentChannel = await Channels.findOne({_id: channelID}).populate({
+      path: 'messages',
+      populate: {path: 'user'}
+    })
+    //Find all public channels to display
   const channels = await Channels.find({private: false})
 
   //Hämta alla channels som är privata för den här användaren.
   const privateChannels = await Channels.find({users: req.user._id})
 
   //Gets the current channel and populates the messages array.
-  const currentChannel = await Channels.findOne({_id: channelID}).populate({
-    path: 'messages',
-    populate: {path: 'user'}
-  })
-  res.render("index.ejs", { channels, currentChannel, privateChannels, loggedInUser });
+  
+  res.render("index.ejs", { channels, currentChannel, privateChannels, loggedInUser });    
+  } catch (error) {
+    res.redirect('/chat')
+  }
+
 };
 
-export const addPublicChannel = async (req, res) => {
-  
+export const getFirstPublicChannel = async (req, res) => {
+  const firstChannel = await Channels.findOne({private: false})
+  res.redirect(`/chat/${firstChannel._id}`)
 }
--- routes/chat.js
+++ routes/chat.js
@@ -1,13 +1,10 @@
 import express from "express";
 //Importera funktioner från controller
-import { renderChat, verifyAccess } from "../controllers/chat.js";
+import { renderChat, verifyAccess, getFirstPublicChannel } from "../controllers/chat.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    //Gör ett databasanrop och lägg in den första?
-    res.redirect("/chat/603f9d2db0c39ee46a9a8a5a")
-});
+router.get("/", getFirstPublicChannel)
 router.get("/:id", verifyAccess, renderChat)
 
 // router.post("/")
